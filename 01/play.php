302aa27298d73771a261da94d6afe1d1f9e28213
ue8d <webmaster@tomoki12.com>
2020-10-30 18:45:24 +0900

    GETリクエストにエスケープ処理を追加

--- 01/play.php
+++ 01/play.php
@@ -1,25 +1,19 @@
-<?php $sample = video/sample.mp4 ?>
+<?php $title = isset($_GET['videoName']) ? htmlspecialchars($_GET['videoName']) : "読み込めませんでした"; ?>
 <!DOCTYPE html>
   <html>
   <head>
     <meta charset="utf-8" />
-    <title>Play</title>
+    <title>Play "<?php echo $title ?>" | ue8d's Videos</title>
     <link rel="stylesheet" type="text/css" href="../lib/video-js.css" />
     <script type="text/javascript">
-      var videoPass = "<?php echo $_GET['videoPass']; ?>";
+      var videoPass = "<?php echo htmlspecialchars($_GET['videoPass']); ?>";
     </script>
     <script src="../lib/video.js"></script>
     <script src="index.js"></script>
   </head>
   <body style="background-color:white">
     <p>
-      <?php
-        if(isset($_GET["videoName"])){
-          print($_GET["videoName"]);
-        }else {
-          echo "読み込めませんでした";
-        }
-      ?>
+      <?php $title ?>
     </p>
     <video id="video1" class="video-js vjs-default-skin vjs-big-play-centered"></video>
     <br>
