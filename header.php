a83da8dd2c5825182658c2f98878b4481ff3bac9
ue8d <webmaster@tomoki12.com>
2020-11-11 00:36:46 +0900

    項目の編集

--- header.php
+++ header.php
@@ -6,12 +6,15 @@
     <ul class="header">
         <li class="header"><a class="header" href="/about.php">ABOUT</a></li>
         <li class="header"><a class="header" href="/contact.php">CONTACT</a></li>
-        <li class="header"><a class="header" href="/dbInsert.php">DBINSERT</a></li>
         <?php
+        if ($_SESSION["role"] == 999) {
+            echo '<li class="header"><a class="header" href="/dbInsert.php">DBINSERT</a></li>';
+        }
         if($_SESSION["id"] != null) {
             echo '<li class="header"><a class="header" href="/logout.php">LOGOUT</a></li>';
         }else {
             echo '<li class="header"><a class="header" href="/login.php">LOGIN</a></li>';
+            echo '<li class="header"><a class="header" href="/register.php">SIGN UP</a></li>';
         }
         ?>
     </ul>
